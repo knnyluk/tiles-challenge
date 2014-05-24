b5eae21ac3088d4a225171dbb301257d0891e525
Ken Luk <knnyluk@gmail.com>
2014-05-24 17:38:00 -0500

    click creation delegated to ClickWorker

--- app/controllers/clicks_controller.rb
+++ app/controllers/clicks_controller.rb
@@ -5,16 +5,7 @@ class ClicksController < ApplicationController
   end
 
   def create
-    @click = Click.new(click_params)
-    
-    respond_to do |format|
-      if ClicksController.random_error?
-        @click.save
-        format.json { head :no_content }
-      else
-        format.json { render json: @click.errors, status: :unprocessable_entity }
-      end
-    end
+    ClicksWorker.perform_async(params)
   end
 
   private
--- app/models/workers/clicks_worker.rb
+++ app/models/workers/clicks_worker.rb
@@ -2,7 +2,16 @@ class ClicksWorker
   include Sidekiq::Worker
 
 def perform
-  
+  @click = Click.new(click_params)
+    
+  respond_to do |format|
+    if ClicksController.random_error?
+      @click.save
+      format.json { head :no_content }
+    else
+      format.json { render json: @click.errors, status: :unprocessable_entity }
+    end
+  end
 end
 
 end
