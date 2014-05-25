6fd6c3538cde4d05ba2be70649bd440ce53065a9
Ken Luk <knnyluk@gmail.com>
2014-05-24 22:20:34 -0500

    config.gem 'redis' to config/environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 require 'sidekiq'
+config.gem 'redis'
 # Initialize the Rails application.
 TilesChallenge::Application.initialize!
