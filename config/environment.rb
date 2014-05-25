17d9dfa7c6e41f4ec45921516a02ae3c538dadbe
Ken Luk <knnyluk@gmail.com>
2014-05-24 22:22:11 -0500

    removed config.gem 'redis' from config/environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,5 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 require 'sidekiq'
-config.gem 'redis'
 # Initialize the Rails application.
 TilesChallenge::Application.initialize!
